a7f66bd0a15fee976ecb7f9d1e7923463b17be18
Syed Hassan <syehassan@gmail.com>
2021-08-02 15:53:44 -0700

    updated code

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const image = document.querySelector('.food')
+const searchForm = document.querySelector('form');
+const searchResultDiv = document.querySelector('.search-result');
+const container = document.querySelector('.container');
+const APP_ID = '5ab4707b';
+const APP_key = '73263b1090f9ef887c11eac5b9ed7d88';
 
-image.addEventListener('click' function increaseSize(params) {
-    image.style.left = 100px;
-})
+
+let searchQuery ='';
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchQuery = e.target.querySelector('input').value;
+    fetchAPI();
+});
+
+async function fetchAPI() {
+    const baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_key}`;
+    const response = await fetch(baseURL);
+    const data = await response.json()
+    console.log(data);
+}
